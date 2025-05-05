770a4b36644fe94b886dd9d0cf5c124087fbbfea
teknixproject <project@teknixcorp.com>
2025-05-05 14:13:41 +0700

    Update index - Add Test

--- components/commons/index.tsx
+++ components/commons/index.tsx
@@ -18,4 +18,5 @@ export const components = {
   navigation: dynamic(() => import('./Navigation')),
   dropdown: dynamic(() => import('./Dropdown')),
   collapse: dynamic(() => import('./Collapse')),
+  test: dynamic(() => import("./Test")),
 };
